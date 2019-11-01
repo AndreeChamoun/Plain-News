09b8485a4511df1813d3daf4d3f5dec11252a373
Tartaros1 <sabahchamoun@hotmail.com>
2019-11-01 12:12:49 +0100

    reversed the date sorting

--- data.php
+++ data.php
@@ -14,12 +14,12 @@ $articles = [
     "authorId" => 10,
     "likecounter" => rand(0,1000)
     ],
-    
+
     [
 		"title" => "U.S. Flag Recalled After Causing 143 Million Deaths",
 		"image" => "/Images/flag.png",
         "content" => "WASHINGTON—Citing a series of fatal malfunctions dating back to 1777, flag manufacturer Annin & Company announced Monday that it would be recalling all makes and models of its popular American flag from both foreign and domestic markets.
-        Representatives from the nation's leading flag producer claimed that as many as 143 million deaths in the past two centuries can be attributed directly to the faulty U.S. models, 
+        Representatives from the nation's leading flag producer claimed that as many as 143 million deaths in the past two centuries can be attributed directly to the faulty U.S. models,
         which have been utilized extensively since the 18th century in sectors as diverse as government, the military, and public education. ",
 		"published" => "2019-02-28",
 		"likes" => "",
@@ -29,7 +29,7 @@ $articles = [
     [
 		"title" => "Entitled Burger King Employee Wants $15 An Hour Just For Dealing With Worst Of America Every Day",
 		"image" => "/Images/burger.png",
-        "content" => "TOLEDO, OH—Appalled by the level of deluded selfishness of millennials in the labor force, citizens expressed shock and disbelief Wednesday at the news that Burger King employee Kayla Werther expects to be paid $15 an hour for the simple task of dealing with the absolute worst of America every day. 
+        "content" => "TOLEDO, OH—Appalled by the level of deluded selfishness of millennials in the labor force, citizens expressed shock and disbelief Wednesday at the news that Burger King employee Kayla Werther expects to be paid $15 an hour for the simple task of dealing with the absolute worst of America every day.
         “This kid thinks she deserves that kind of reward just for doing her stupid, easy job? Give me a break,” said a regular Burger King customer who spoke on condition of anonymity and watched with barely concealed disdain as the “privileged and entitled freeloader” worked a 10-hour shift, which included mopping excreta-coated bathrooms,
         suffering prolonged verbal abuse at the counter, wiping tables strewn with wadded-up dirty napkins and ketchup spills, breathing a mist of aerosolized fryer grease and stale sweat for prolonged periods while stationed at the grill, calling an ambulance for an unconscious homeless man in the parking lot, taking overstuffed bags of dripping trash to the dumpster three times an hour,
         and pausing on an overturned pickle bucket in the supply closet for a five-minute break. ",
@@ -42,7 +42,7 @@ $articles = [
 		"title" => "‘Hey You, Want To Be A Federal Judge?’ Says Mitch McConnell Pointing To Valet In Heritage Foundation Parking Lot",
 		"image" => "/Images/mitch.png",
         "content" => "“WASHINGTON—After realizing there were still judicial appointments that needed to be filled during a meeting with the conservative think tank, Senate Majority Leader Mitch McConnell reportedly pointed to a valet in the Heritage Foundation parking lot Thursday and asked him if he wanted to be a federal judge.
-        “Hey, kid, how’d you like a lifetime appointment on the Ninth Circuit, huh?” asked McConnell, interrupting the 19-year-old temp worker’s protests that he didn’t know anything about the law to tell him that all he needed was “wipe that dumb look off your face” and he could be delivering rulings by the end of the week. 
+        “Hey, kid, how’d you like a lifetime appointment on the Ninth Circuit, huh?” asked McConnell, interrupting the 19-year-old temp worker’s protests that he didn’t know anything about the law to tell him that all he needed was “wipe that dumb look off your face” and he could be delivering rulings by the end of the week.
         “You over 18? You got an ID? That’ll do. Now just hop in this car with me and we’ll head over to the Capitol right now. Remember, abortion’s bad, corporations are good, and as for everything else, you just shut the fuck up and do as your told. Got it?” At press time, after the valet nervously informed McConnell that he was hungover and had illegal drugs in his system, the laughing Senate leader assured him that wouldn’t be an issue. ",
 		"published" => " 2019-03-06",
 		"likes" => "",
@@ -82,7 +82,7 @@ $articles = [
     [
 		"title" => "Nation Perplexed By 16-Year-Old Who Doesn’t Want World To End",
 		"image" => "/Images/greta.png",
-        "content" => "NEW YORK—Following her U.N. address about the existential threat posed by a rapidly warming planet, citizens across the United States confirmed Monday they were perplexed by Swedish climate activist Greta Thunberg, a 16-year-old who apparently has no desire to see the world end. “I know she’s from another country, but she’s still a teenager, right?” said 33-year-old Sophia Williams of Kenosha, WI, her confusion reportedly shared by millions of Americans who recalled that during their own adolescence they had hated the world and everyone in it and had felt the end couldn’t come fast enough. 
+        "content" => "NEW YORK—Following her U.N. address about the existential threat posed by a rapidly warming planet, citizens across the United States confirmed Monday they were perplexed by Swedish climate activist Greta Thunberg, a 16-year-old who apparently has no desire to see the world end. “I know she’s from another country, but she’s still a teenager, right?” said 33-year-old Sophia Williams of Kenosha, WI, her confusion reportedly shared by millions of Americans who recalled that during their own adolescence they had hated the world and everyone in it and had felt the end couldn’t come fast enough.
         “I don’t understand why a high school kid like her isn’t locking herself in her bedroom and writing bad poetry about how nothing matters and everything is meaningless. Instead of going around the world and giving speeches in which she urges people to save the planet, shouldn’t she be commiserating with her friends about how pointless life is and how we’d all be better off dead? I just don’t get it.” At press time, an online survey found that a majority of Americans agree someone should make sure Thunberg’s parents are aware of her unusual behavior in case she needs to seek help.",
 		"published" => "2019-09-10",
 		"likes" => "",
@@ -92,8 +92,8 @@ $articles = [
     [
 		"title" => "Mark Zuckerberg Argues High Taxes On Wealthy Could Stifle Innovation In Human Suffering",
 		"image" => "/Images/mark.png",
-        "content" => "PALO ALTO, CA—Warning that progressive Democrats’ plans to fund government projects through tax increases on billionaires would have significant negative consequences, Facebook founder Mark Zuckerberg reportedly argued Monday that high taxes on the wealthy could stifle innovation in human suffering. 
-        “If there’s anything that the rise of Silicon Valley has taught us, it’s that in order to make trailblazing breakthroughs in cruelty and technological oppression, we need to empower the private sector,” said Zuckerberg, adding that without the promise of being able to fund and ultimately make money off of his inventions, 
+        "content" => "PALO ALTO, CA—Warning that progressive Democrats’ plans to fund government projects through tax increases on billionaires would have significant negative consequences, Facebook founder Mark Zuckerberg reportedly argued Monday that high taxes on the wealthy could stifle innovation in human suffering.
+        “If there’s anything that the rise of Silicon Valley has taught us, it’s that in order to make trailblazing breakthroughs in cruelty and technological oppression, we need to empower the private sector,” said Zuckerberg, adding that without the promise of being able to fund and ultimately make money off of his inventions,
         he would have never worked to erode civil liberties, invade people’s privacy on a grand scale, and manipulate the democratic process by creating Facebook. “If we want the kind of brutal, intrusive solutions that the market can provide, we can’t punish those who have risen to the top based on their genius at perpetuating human suffering. ",
 		"published" => "2019-12-30",
 		"likes" => "",
@@ -103,8 +103,8 @@ $articles = [
     [
 		"title" => "NASA Frantically Announces Mission To Earth’s Core After Accidentally Launching Rocket Upside Down",
 		"image" => "/Images/rocket.png",
-        "content" => "HOUSTON—Rushing into a press conference mere minutes after lift-off, NASA officials frantically announced a mission to the Earth’s core Friday after accidentally launching a Atlas V rocket upside down. “Today, I’m excited to announce that we’ve successfully launched—let’s see. Well, I guess we’ll just call this the first-ever manned mission to explore this planet’s core,” 
-        said sweat-covered and visibly panicked NASA administrator Jim Bridenstine, who paused momentarily to put up a crudely drawn diagram labeled “Core Shot 2020” depicting the rocket burrowing through the planet’s crust towards its 10,000-degree-Fahrenheit inner core. “Thanks to the ship’s powerful boosters and its pointed tip, we anticipate the rocket might conceivably break through the Earth’s mantle and reach its destination within the year. 
+        "content" => "HOUSTON—Rushing into a press conference mere minutes after lift-off, NASA officials frantically announced a mission to the Earth’s core Friday after accidentally launching a Atlas V rocket upside down. “Today, I’m excited to announce that we’ve successfully launched—let’s see. Well, I guess we’ll just call this the first-ever manned mission to explore this planet’s core,”
+        said sweat-covered and visibly panicked NASA administrator Jim Bridenstine, who paused momentarily to put up a crudely drawn diagram labeled “Core Shot 2020” depicting the rocket burrowing through the planet’s crust towards its 10,000-degree-Fahrenheit inner core. “Thanks to the ship’s powerful boosters and its pointed tip, we anticipate the rocket might conceivably break through the Earth’s mantle and reach its destination within the year.
         I also cannot stress enough the bravery of the two astronauts inside the rocket, who are presumably okay with all of this. You know, some might even say a NASA expedition to the Earth’s core was long overdue.” At press time, NASA officials expressed hope that the rocket would exit the other side of the Earth and proceed on its previously planned mission to restock the International Space Station by 2021. ",
 		"published" => "2019-12-23",
 		"likes" => "",
@@ -133,5 +133,5 @@ $authors = [
     14 => [
         "name" => "Marie Sanders",
         "authimage" => "/images/marie.png"
-    ]      
+    ]
     ];
--- functions.php
+++ functions.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 function Sortbydate(array $a, array $b): int
 {
-    return strtotime($a["published"]) <=> strtotime($b["published"]);
+    return strtotime($b["published"]) <=> strtotime($a["published"]);
 }
 // ^ Sorts the date of the articles
